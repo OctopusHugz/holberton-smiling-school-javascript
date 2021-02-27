a7b252be79570bcce81d5eb5286b07ee341dfcc3
OctopusHugz <colsonscott53@gmail.com>
2021-02-27 00:32:29 +0000

    Added inital changes to load quotes from API

---
+++ scripts.js
@@ -0,0 +1,16 @@
+$(() => {
+  $.getJSON("https://smileschool-api.hbtn.info/quotes", (response) => {
+    response.forEach((user) => {
+      let userName = user.name;
+      let userTitle = user.title;
+      let userQuote = user.text;
+      let userPicURL = user.pic_url;
+      let carouselQuoteP = $(`p.quote-${user.id}`);
+      let carouselNameH5 = $(`h5.name-${user.id}`);
+      let carouselTitleP = $(`p.title-${user.id}`);
+      carouselQuoteP.html(userQuote);
+      carouselNameH5.html(userName);
+      carouselTitleP.html(userTitle);
+    });
+  });
+});
